1955c429c92d92337d63e9977a83909e6af161f0
webresurs <76422777+webresurs@users.noreply.github.com>
2020-12-21 13:56:14 +0500

    Create translations.php

---
+++ Locale/ru_RU/translations.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+		'Assigned Group' => 'Назначенная группа',
+    'Assigned Group:' => 'Назначенная группа:',
+    'Other Assignees' => 'Другие задания',
+    'Other Assignees:' => 'Другие задания:',
+    'Send a task by email to assigned group members' => 'Отправить задачу назначенным участникам группы по электронной почте',
+		'Send a task by email to the other assignees for a task.' => 'Отправить задачу по электронной почте другим, назначенным в задаче',
+		'Send email notification of impending due date to Group Members Assigned' => 'Отправить уведомление по электронной почте о приближающемся сроке выполнения назначенным членам группы',
+		'Send email notification of impending due date to the other assignees of a task' => 'Отправлять по электронной почте уведомление о приближающемся сроке выполнения другим исполнителям задачи',
+    'Assign the task to a specific group' => 'Назначьте задачу определенной группе'
+);
